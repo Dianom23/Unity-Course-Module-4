8e11640ee6174ad6e3fec14b6a963057e85205dc
Dianom <dmitry.sh23@gmail.com>
2026-01-10 22:56:47 +0300

    Lesson 5

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+[SelectionBase]
+public class Door : MonoBehaviour
+{
+    // Компонент аниматора двери
+    private Animator _animator;
+    // Флаг состояния двери
+    private bool _isOpen = false;
+    // Флаг, указывающий, что игрок рядом с дверью
+    private bool _isPlayerNear = false; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Получаем компонент Animator на этом объекте
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, является ли объект игроком
+        if (other.CompareTag("Player"))
+        {
+            // Устанавливаем флаг, что игрок рядом с дверью
+            _isPlayerNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Проверяем, является ли объект игроком
+        if (other.CompareTag("Player"))
+        {
+            // Сбрасываем флаг, что игрок рядом с дверью
+            _isPlayerNear = false;
+        }
+    }
+
+    private void OnInteract()
+    {
+        // Проверяем, что дверь закрыта и игрок рядом
+        if (_isOpen == false && _isPlayerNear == true)
+        {
+            // Запускаем анимацию открытия двери через Trigger "Open"
+            _animator.SetTrigger("Open");
+            // Устанавливаем флаг, что дверь теперь открыта
+            _isOpen = true;
+        }
+    }
+}
